07965b28f36c8a70aaa8807f37a299aa44459723
Ariinto <142360875+arieljorge@users.noreply.github.com>
2025-08-24 13:02:06 -0300

    add: spring initializer files
    adding the initial project files

---
+++ src/main/java/com/github/abstract_leaf_org/neotoon_api/NeotoonApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.abstract_leaf_org.neotoon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeotoonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeotoonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/abstract_leaf_org/neotoon_api/NeotoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.abstract_leaf_org.neotoon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeotoonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
